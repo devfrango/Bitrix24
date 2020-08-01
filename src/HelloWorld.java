28321401ed5e02c8b96b805b81642f6f8aa2e711
Frankie(devfrango) <devfrango@gmail.com>
2020-08-02 03:46:16 +0700

    Fixed print method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.printnl("Woah! I added this from github!");
+        System.out.println("Woah! I added this from github!");
     }
 }
